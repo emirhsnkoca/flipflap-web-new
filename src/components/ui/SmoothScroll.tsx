564580f25c22924a943b8ec809286d876652f39a
Emir Hasan Koca <emirhasankoca@gmail.com>
2025-11-30 05:36:27 +0300

    build settings

--- src/components/ui/SmoothScroll.tsx
+++ src/components/ui/SmoothScroll.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useEffect } from 'react';
+import { useEffect } from 'react';
+import type { ReactNode } from 'react';
 import Lenis from 'lenis';
 import 'lenis/dist/lenis.css';
 
@@ -7,8 +8,8 @@ export const SmoothScroll = ({ children }: { children: ReactNode }) => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      direction: 'vertical',
-      gestureDirection: 'vertical',
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
       smoothWheel: true,
       wheelMultiplier: 1,
       touchMultiplier: 2,
